baa962ab13434ef0b298e0e1ba5f596a5349d0c4
WOODBRICKS <bungbale@gmail.com>
2020-10-10 12:09:54 +0900

    1010 updated4

--- Sandbox/main.swift
+++ Sandbox/main.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 // 1번. 1-100 사이의 3, 5의 배수의 합
+/*
 var total = 0
 for num in 1...100{
     if num%3 == 0 && num%5 == 0{
@@ -15,8 +16,15 @@ for num in 1...100{
     }
 }
 print(total)
+*/
+
+let hundred = Array(1...100)
+var modify = hundred.filter({ (value: Int) -> Bool in return (value % 3 == 0 && value % 5 == 0) })
+var answer = modify.reduce(0){(s1, s2) in s1+s2}
+print(answer)
 
 // 2번. 두 수 사이의 정수의 합
+/*
 func add(_ start: Int, _ final: Int) -> Int{
     var total = 0
     for num in start...final{
@@ -25,8 +33,15 @@ func add(_ start: Int, _ final: Int) -> Int{
     return total
 }
 print(add(1,10))
-
+*/
+func add(_ start: Int, _ final: Int) -> Int{
+    var list5 = Array(start...final)
+    var total = list5.reduce(0){ (s1, s2) in s1 + s2 }
+    return total
+}
+ 
 // 3번. 1-50 사이의 7의 배수가 아닌 수의 합
+/*
 var total2 = 0
 for num in 1...50{
     if num%7 != 0{
@@ -34,8 +49,14 @@ for num in 1...50{
     }
 }
 print(total2)
+*/
+let list3 = Array(1...50)
+let modify2 = list3.filter( { (value: Int) -> Bool in return (value % 7 != 0) } )
+var answer3 = modify2.reduce(0){(s1, s2) in s1+s2}
 
+ 
 // 4번. 두 수 사이의 정수의 3제곱의 합
+/*
 func addSquare(_ start: Int, _ final: Int) -> Int{
     var total = 0
     for num in start...final{
@@ -44,6 +65,15 @@ func addSquare(_ start: Int, _ final: Int) -> Int{
     return total
 }
 print(addSquare(1,10))
+*/
+ 
+func addSquare2(_ start: Int, _ final: Int) -> Int{
+    var list5 = Array(start...final)
+    var modifylist = list5.map( { (value: Int) -> Int in return value * 3 } )
+    var total = modifylist.reduce(0){ (s1, s2) in s1 + s2 }
+    return total
+}
+addSquare2(3,10)
 
 // 5번. string list를 받으면 글자 길이의 합
 let words = ["Sean", "Mike", "Hello", "Square"]
