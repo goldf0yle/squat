256c083987bc76fc16752088c6cdb93e7e588683
WOODBRICKS <bungbale@gmail.com>
2020-10-23 11:19:32 +0900

    1023 updated 2

---
+++ Application/squatMotionClassifier/Pods/Target Support Files/Pods-squatMotionClassifier-squatMotionClassifierUITests/Pods-squatMotionClassifier-squatMotionClassifierUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_squatMotionClassifier_squatMotionClassifierUITests : NSObject
+@end
+@implementation PodsDummy_Pods_squatMotionClassifier_squatMotionClassifierUITests
+@end
